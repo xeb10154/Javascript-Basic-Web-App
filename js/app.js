f720828cdbdf50858ba262051650a96769626ea9
Raymond Yau <raymondccyau@gmail.com>
2018-10-14 23:23:14 +0100

    added radio buttons

--- js/app.js
+++ js/app.js
@@ -1,30 +1,56 @@
 document.addEventListener('DOMContentLoaded', () => {
-console.log("This works")
-  const handleSubmit = ((event) => {
-    event.preventDefault()
+  console.log("This works")
 
-    const title = event.target.title.value;
-    const artist = event.target.artist.value;
-    const genre = event.target.genre.value;
+  const selectDOM = ((domObject) => {
+    return document.querySelector(domObject)
+  })
+
+  const newTextElement = ((tag, content) => {
+    element = document.createElement(tag);
+    element.textContent = content;
+    return element
+  })
+
+  const deleteAll = (() => {
+    const listOfAlbums = selectDOM('#listOfAlbums');
+    while(listOfAlbums.firstChild){
+      listOfAlbums.removeChild(listOfAlbums.firstChild)
+    }
+  })
+
+  const addItem = ((title, artist, genre, download) => {
 
     const wrapper = document.createElement('div')
 
-    const titleElement = document.createElement('h2');
-    titleElement.textContent = title;
+    wrapper.appendChild(newTextElement('h2', title))
+    wrapper.appendChild(newTextElement('p', artist))
+    wrapper.appendChild(newTextElement('p', genre))
+    wrapper.appendChild(newTextElement('p', download))
 
-    const artistElement = document.createElement('p');
-    titleElement.textContent = artist;
+    const listDiv = selectDOM('#listOfAlbums');
+    listDiv.appendChild(wrapper)
+  })
 
-    const genreElement = document.createElement('p');
-    titleElement.textContent = genre;
+  const handleSubmit = ((event) => {
+    event.preventDefault()
+
+    const title = event.target.title.value
+    const artist = event.target.artist.value
+    const genre = event.target.genre.value
+    const download = event.target.download.value
+
+    addItem(title, artist, genre, download)
 
     const form = event.target
     form.reset()
-
   })
 
-  const form = document.querySelector('form')
+  const form = selectDOM('form')
   form.addEventListener('submit', handleSubmit)
+
+  const deleteBtn = selectDOM('#deleteAll')
+  deleteBtn.addEventListener('click', deleteAll)
+
 
 
 
